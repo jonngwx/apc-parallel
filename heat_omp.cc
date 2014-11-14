f937a313d1e80ed543a70e26cda2d2f8e3a8fea4
Jonathan Ng <jonngwx@hopper09.(none)>
2014-11-14 14:12:26 -0800

    omp version

---
+++ heat_omp.cc
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "euler.h"
+#include "utils.h"
+#include "heat_eq.h"
+#include <time.h>
+#include <string.h>
+#include <omp.h>
+#define PI 3.14159265358979
+
+int main(int argc, const char * argv[]){
+
+  Model * model;
+  Integrator *integrator;
+  
+  if (argc != 3){
+    printf("Requires argument <nx> <nthreads>\n");
+    return 0;
+  }
+
+  int nx = atoi(argv[1]);
+  int ny = nx;
+  int nthreads = atoi(argv[2]);
+
+  //input checking
+  if (nx < 2){
+    printf("grid is too small. requires nx >= 3 for nontrivial solution");
+    return 0;
+  }
+  if (nthreads < 1){
+    printf("needs more than one thread to start...");
+    return 0;
+  }
+  
+  omp_set_num_threads(nthreads);
+  clock_t time;
+  time = clock();
+  printf("%1d\n",omp_get_num_threads());
+  return 0;
+  const double kappa = 1.;
+  
+  const double dx = PI/(nx-1);
+  const double dt = dx*dx/4/kappa*.95; // stability condition.
+
+  const double tmax = .5*PI*PI/kappa;
+  double t = 0.;
+  model = new Heat_Eq(kappa,nx,ny);
+  integrator = new Euler(dt,*model);
+
+
+  while (t<tmax){
+    integrator->Step(t, (*model).get_grid());
+    t = t+dt;
+  }
+  time = clock() - time;
+  //printf("%1.8f\n",((float)time)/CLOCKS_PER_SEC);
+
+  char filename[80];
+  strcpy(filename,"heat_out_");
+  strcat(filename,argv[1]);
+  double Tave;
+  Tave = average((*model).get_grid(), nx,ny);
+  output_2d((*model).get_grid(),nx,ny,filename);
+  printf("%1.8f, %10.8f\n",Tave, ((float)time)/CLOCKS_PER_SEC);
+  delete model;
+  delete integrator;
+  return 0;
+}
