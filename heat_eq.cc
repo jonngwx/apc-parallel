d2231f74ccf102aa366f93277a27495d379ecd6f
Jon <jon@ubuntu.(none)>
2014-11-15 16:54:27 -0500

    fixed errors from revert

--- heat_eq.cc
+++ heat_eq.cc
@@ -35,23 +35,12 @@ Heat_Eq::~Heat_Eq(){
 int Heat_Eq::rhs(double t, const double * const *T, double ** fx) const{
 #pragma omp parallel for
   for (int i = 1; i < (nx - 1); i++){ // use 1,n-1 due to boundary terms
-<<<<<<< HEAD
-#pragma omp for
-    for (int j = 1; j < (ny - 1); j+=4){
-=======
     for (int j = 1; j < (ny - 1); j++){
->>>>>>> parent of 9dabbb2... moving for loop
-      fx[i][j] = kappa * nabla_squared(T,x,y,i,j);
-      fx[i][j+1] = kappa * nabla_squared(T,x,y,i,j+1);
-      if ((j+2) == ny-2){
-	continue;
+	fx[i][j] = kappa * nabla_squared(T,x,y,i,j);
       }
-      fx[i][j+2] = kappa * nabla_squared(T,x,y,i,j+2);
-      fx[i][j+3] = kappa * nabla_squared(T,x,y,i,j+3);
-    }
-  }// boundary conditions
-  bcx(t, T, fx);
-  bcy(t, T, fx);
+    }// boundary conditions
+    bcx(t, T, fx);
+    bcy(t, T, fx);
 
   return 0;
 }
